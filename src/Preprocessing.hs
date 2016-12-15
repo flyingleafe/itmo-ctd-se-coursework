4c595c28f402bc8f6765ab3cd715a1a7cc29795a
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2016-12-15 14:11:13 +0300

    [Refactor] Preprocessing.hs

--- src/Preprocessing.hs
+++ src/Preprocessing.hs
@@ -1,33 +1,29 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-
 module Preprocessing where
 
-import           Prelude hiding (FilePath)
+import           Control.Monad                (mapM)
+import qualified Data.Map                     as M
+import           Data.Maybe                   (fromJust)
+import qualified Data.Text                    as T
 import           Data.Type.Natural
-import           GHC.TypeLits
 import qualified Data.Vector.Sized            as VS
-import           Numeric.LinearAlgebra.Static
-import           Data.Maybe (fromJust)
-import qualified Data.Text.IO as TIO
-import           Model
-import           Filesystem.Path
 import           Filesystem
-import qualified Data.Text as T
-import qualified Data.Map as M
-import           Control.Monad (mapM)
+import           Filesystem.Path              (FilePath, hasExtension)
+import           GHC.TypeLits
+import           Model
+import           Numeric.LinearAlgebra.Static
+import           Universum                    hiding (FilePath)
 
 class Preprocessing a where
     getCollection :: (KnownNat w, KnownNat d) => a -> IO (Maybe (DocCollection w d))
 
 newtype TextDirectoryParser = TDParser FilePath
 
-newtype AcidStateParser = ASParser String -- String is a hole here
+newtype AcidStateParser = ASParser Text -- String is a hole here
 
-countTokens :: T.Text -> M.Map T.Text Integer
+countTokens :: T.Text -> M.Map Text Integer
 countTokens = undefined . T.words
 
-mergeCounts :: (KnownNat w, KnownNat d) => [M.Map T.Text Integer] -> DocCollection w d
+mergeCounts :: (KnownNat w, KnownNat d) => [M.Map Text Integer] -> DocCollection w d
 mergeCounts = undefined
 
 instance Preprocessing TextDirectoryParser where
