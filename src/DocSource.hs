463fd10f15a4ad3059b60e80d3bd8d544609dbfb
Anton Belyy <anton.belyy@gmail.com>
2017-01-09 10:14:26 +0300

    [Refactor] Move NLP module into existing DocSource

--- src/DocSource.hs
+++ src/DocSource.hs
@@ -1,50 +1,45 @@
 module DocSource
        ( runTDSource
+       , TfIdfCollection
        ) where
 
-import           Control.Monad                (mapM, unless)
-import           Control.Monad.Except         (ExceptT, MonadError, runExceptT,
-                                               throwError)
-import qualified Data.Map                     as M
-import           Data.Maybe                   (fromJust)
-import           Data.Proxy                   (Proxy (..))
-import qualified Data.Text                    as T
-import           Data.Type.Natural
-import           Filesystem
-import           Filesystem.Path              (FilePath, hasExtension)
-import           Formatting                   (sformat, shown, stext, (%))
-import           GHC.TypeLits
-import           Numeric.LinearAlgebra.Data
-import           Prelude                      (show)
-import           Universum                    hiding (FilePath, show)
+import           Control.Monad            (mapM, unless)
+import           Data.Map                 (keys)
+import           Data.Text                (pack, unpack)
+import           Formatting               (sformat, shown, (%))
+import           NLP.Similarity.VectorSim (mkDocument, tf_idf)
+import           NLP.Types                (Corpus (..), mkCorpus)
+import           System.Directory         (doesDirectoryExist, listDirectory)
+import           System.FilePath          ((</>))
+import           Universum
 
-import           Config                       (TMConfig (..))
-import           Model
+import           Config                   (TMConfig (..))
 import           Types
 
 newtype TextDirectorySource a = TDSource
     { getTDSource :: Base TMError a
-    } deriving (Functor, Applicative, Monad, MonadIO,
-                MonadError TMError)
+    } deriving (Functor, Applicative, Monad, MonadIO)
 
-countTokens :: T.Text -> M.Map T.Text Integer
-countTokens = foldl' (\m w -> M.insertWith (+) w 1 m) M.empty . T.words
+type DocCollection = [[Text]]
+type TfIdfCollection = [[Double]]
 
-mergeCounts :: [M.Map T.Text Integer] -> DocCollection
-mergeCounts ms = map listT ms
-    where listT m = SBOW $ M.toList $ M.mapKeys (dict M.!) m
-          dict = M.fromAscList $ (`zip` [1..]) $ M.keys $ M.unionsWith (+) ms
+preprocess :: Text -> [Text]
+preprocess = words
 
 getTDCollection :: FilePath -> TextDirectorySource DocCollection
-getTDCollection fp = do
-    isOk <- liftIO $ isDirectory fp
-    unless isOk $ throwError $
-        sformat ("No directory on path "%shown) fp
+getTDCollection = fmap (map preprocess) . (mapM readFile =<<) . liftIO . getDirRec
+    where
+    getDirRec fp = do
+        isDir <- doesDirectoryExist fp
+        if isDir
+        then fmap concat $ map (fp </>) <$> listDirectory fp >>= mapM getDirRec
+        else return [fp]
 
-    docFilePaths <- filter (`hasExtension` "txt") <$>
-                    liftIO (listDirectory fp)
-    counts <- mapM (fmap countTokens <$> liftIO . readTextFile) docFilePaths
-    return $ mergeCounts counts
+tfIdfVectorize :: DocCollection -> TfIdfCollection
+tfIdfVectorize dic = map docVectorize dic where
+    docVectorize doc = map (\w -> tf_idf w (mkDocument doc) corpus) terms
+    terms = keys (corpTermCounts corpus)
+    corpus = mkCorpus dic
 
-runTDSource :: FilePath -> Base TMError DocCollection
-runTDSource = getTDSource . getTDCollection
+runTDSource :: FilePath -> Base TMError TfIdfCollection
+runTDSource = fmap tfIdfVectorize . getTDSource . getTDCollection
