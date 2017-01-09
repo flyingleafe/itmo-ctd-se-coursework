9e677e9ff0fc2d7915e46fe7f5d9b3d1a843c00f
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2017-01-09 20:26:40 +0300

    Add proper CLI params

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,54 @@
 module Main where
 
-import           Data.Default (def)
-import           Data.String  (fromString)
+import           Data.Default        (def)
+import           Data.String         (fromString)
+import           Options.Applicative (Parser, ParserInfo, auto, command, execParser,
+                                      fullDesc, help, helper, info, long, metavar, option,
+                                      progDesc, short, strOption, subparser, value)
 import           Universum
 
 import           DocSource
 import           Model
 import           OutputSink
 import           Types
+import           Web.Server          (webServer)
+
+data AppMode = CLIMode { datasetDir :: FilePath }
+             | ServerMode { listenPort :: Word16 }
+
+appModeParser :: Parser AppMode
+appModeParser = subparser $
+    command "cli" (info cliOpts $ progDesc "Run once in CLI mode") <>
+    command "serve" (info serveOpts $ progDesc "Serve HTTP API to interact with client")
+
+cliOpts :: Parser AppMode
+cliOpts = CLIMode <$>
+    strOption (long "dataset-dir" <>
+               short 'd' <>
+               metavar "FILEPATH" <>
+               help "Path to dataset")
+
+serveOpts :: Parser AppMode
+serveOpts = ServerMode <$>
+    option auto (long "port" <>
+                 short 'p' <>
+                 metavar "PORT" <>
+                 value 8090 <>
+                 help "Port to serve")
+
+appModeInfo :: ParserInfo AppMode
+appModeInfo = info (helper <*> appModeParser) $
+    fullDesc `mappend` progDesc "Server part of document-clustering application"
+
+pipeline :: FilePath -> Base ()
+pipeline fp = () <$ runTDSource fp  -- >=> runMockModel >=> runMockSink
 
 main :: IO ()
 main = do
-    (path:_) <- getArgs
-    let fp = fromString path
-    res <- runBase def $ runTDSource fp -- >>= runMockModel >>= runMockSink
+    mode <- execParser appModeInfo
+    res <- runBase def $ case mode of
+        CLIMode path    -> pipeline path
+        ServerMode port -> webServer port
     case res of
-        Left err -> putText $ "Error: " <> err
         Right _  -> putText "Finished!"
+        Left err -> putText $ "Error: " <> err
